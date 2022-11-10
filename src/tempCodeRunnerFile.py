181872908cb2c616efddaa000920a5c8c0ac8704
Shruti Sharma <shruti.sharma.sky@gmail.com>
2022-11-10 10:09:08 +0530

    removed the extra file

--- src/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def add_content_from(listfrom, listto):
-    content = []
-    for i in listfrom:
-        if type(i) == list:
-            add_content_from(i, content)
-        else:
-            content.append(i)
-    for i in content:
-        listto.append(i)
